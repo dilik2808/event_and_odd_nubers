c48f5866e68809d128da03671da3e8b8628ffbf1
dilik2808 <dilik2808@gmail.com>
2022-10-05 18:41:36 +0500

    test

--- sum_even.py
+++ sum_even.py
@@ -5,3 +5,14 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+
+var_int=6723
+sum_even =0
+x1= var_int%10 #3
+y= var_int%100
+x2=y//10 #2
+z=var_int%1000 #723
+x3=z//100 #7
+x4=var_int//1000 #6
+sum_even =(x1+1)%2+(x2+1)%2+(x3+1)%2+(x4+1)%2
+print(sum_even)
