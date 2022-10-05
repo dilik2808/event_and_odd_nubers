010be5ca9cac50a4b776ba0d63dee31577a95a71
dilik2808 <dilik2808@gmail.com>
2022-10-05 15:57:34 +0500

    test5

--- number_even.py
+++ number_even.py
@@ -1,7 +1,9 @@
 #A four-digit integer is given. Find the number of even digits in it.
+from multiprocessing import Event
+
+
 var_int= 3588
 #Create a variable "var_int" and assign it a four-digit integer value.
 var_int= var_int//2
+print(var_int)
 #Print the number of even digits in the variable "var_int".
-
-print (var_int)
