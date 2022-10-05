b6d1865dca3645259018160be27d57dbc9ee18d2
dilik2808 <dilik2808@gmail.com>
2022-10-05 12:22:59 +0500

    birinchisi

--- is_even.py
+++ is_even.py
@@ -1,5 +1,6 @@
 #Create a variable "number" and assign it a integer value.
-number=18
+number=1
 #Print 1 if the number is even, otherwise print 0.
-number= number//16
-print(number)
+number2= (number+1)%2
+
+print(number2)
